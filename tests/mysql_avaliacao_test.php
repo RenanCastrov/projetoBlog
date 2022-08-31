42c26378a2d840cca616bc801fd1c6f8b295b6f3
renan <renancastroandrade31@gmail.com>
2022-08-31 11:47:10 -0300

    Atualização 31/08/22

--- tests/mysql_avaliacao_test.php
+++ tests/mysql_avaliacao_test.php
@@ -6,32 +6,81 @@
     require_once '../core/mysql.php';
 
     //Aqui foi usado para fazer testes dos comandos de inserção, atualização, busca e exclusão de dados.
-    buscar_test();
-    delete_test(1);
+    //insere dados
+    insert_teste('Renan', 'renanzin@ifsp.edu.br', '88856');
+    buscar_teste();
+    insert_teste2(2, 'Malditos furadores de fila', 'Sacanagem', '2022-05-08');
+    buscar_teste2();
+    insert_teste3(2, 'Sorvete é mó bom', 1, 1);
+    buscar_teste3();
 
-    //Teste inserção banco de dados
-    function insert_test($nota, $comentario, $post_id, $usuario_id) : void
-    {
-        $dados = ['nota'  => $nota,'comentario' => $comentario, 'post_id' => $post_id, 'usuario_id' => $usuario_id];
-        insere('avaliacao',$dados);
+    //atualiza dados
+    update_teste(2, 'murilo', 'silva@gmail.com');
+    buscar_teste();
+    update_teste2(2, 'UHUUUUUUUU', 'lindeza');
+    buscar_teste2();
+    update_teste3(2, 10, 'nheu');
+    buscar_teste3();
+
+    //delete dados
+    delete_teste3(2);
+    buscar_teste3();
+    delete_teste2(2);
+    buscar_teste2();
+
+    //insert
+    function insert_teste($nome, $email, $senha) : void {
+        $dados = ['nome' => $nome, 'email' => $email, 'senha' => $senha];
+        insere('usuario', $dados);
     }
-    //Teste select banco de dados
-    function buscar_test() : void
-    {
-        $avaliacoes = buscar('avaliacao',['id', 'comentario', 'nota', 'usuario_id', 'post_id'], [], '');
-        print_r($avaliacoes);
+    function insert_teste2($usuario_id, $titulo, $texto, $data_postagem) : void {
+        $dados = ['usuario_id' => $usuario_id,'titulo' => $titulo, 'texto' => $texto, 'data_postagem' => $data_postagem];
+        insere('post', $dados);
     }
-    //Teste update banco de dados
-    function update_test($id, $nota, $comentario, $post_id, $usuario_id) : void
-    {
-        $dados = ['nota'  => $nota,'comentario' => $comentario,'post_id' => $post_id,'usuario_id' => $usuario_id];
+    function insert_teste3($nota, $comentario, $usuario_id, $post_id) : void {
+        $dados = ['nota' => $nota, 'comentario' => $comentario, 'usuario_id' => $usuario_id, 'post_id' => $post_id];
+        insere('avaliacao', $dados);
+    }
+
+    //buscar
+    function buscar_teste() : void {
+        $usuarios = buscar('usuario', ['id', 'nome', 'email', 'senha'], [], '');
+        print_r($usuarios);
+    }
+    function buscar_teste2() : void {
+        $usuarios = buscar('post', ['id', 'titulo', 'texto', 'data_postagem'], [], '');
+        print_r($usuarios);
+    }
+    function buscar_teste3() : void {
+        $usuarios = buscar('avaliacao', ['id', 'nota', 'comentario'], [], '');
+        print_r($usuarios);
+        echo "<br><hr>";
+    }
+
+    //updates
+    function update_teste($id, $nome, $email) : void {
+        $dados = ['nome' => $nome, 'email' => $email];
         $criterio = [['id', '=', $id]];
-        atualiza('avaliacao',$dados,$criterio);
+        atualiza('usuario', $dados, $criterio);
     }
-    //Teste delete banco de dados
-    function delete_test($id) : void
-    {
+    function update_teste2($id, $titulo, $texto) : void {
+        $dados = ['titulo' => $titulo, 'texto' => $texto];
         $criterio = [['id', '=', $id]];
-        delete('avaliacao',$criterio);
+        atualiza('post', $dados, $criterio);
+    }
+    function update_teste3($id, $nota, $comentario) : void {
+        $dados = ['nota' => $nota, 'comentario' => $comentario];
+        $criterio = [['id', '=', $id]];
+        atualiza('avaliacao', $dados, $criterio);
+    }
+
+    //delete
+    function delete_teste3($id) {
+        $criterio = [['id', '=', $id]];
+        deleta('avaliacao', $criterio);
+    }
+    function delete_teste2($id) {
+        $criterio = [['id', '=', $id]];
+        deleta('post', $criterio);
     }
 ?>
